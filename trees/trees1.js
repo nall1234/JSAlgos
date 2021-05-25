990fb27e01d4f1eeb5f9203841027040892f90ec
Evan Gardner <gardner264@hotmail.com>
2021-05-25 13:37:53 -0400

    started on fundamentals 2

--- trees/trees1.js
+++ trees/trees1.js
@@ -9,7 +9,7 @@ class BST {
     constructor() {
         this.root = null;
     }
-    add (value,) {
+    add (value) {
         if (this.root == null){
             this.root = new Node(value);
             return this;
@@ -102,8 +102,47 @@ class BST {
         var rightside = this.size(current_node.right);
         return 1 + leftside + rightside;
     }
+
+    heightHelper(current_node){
+        
+        if (current_node == null){            
+            return 0;
+        }
+        
+        let leftheight = this.heightHelper(current_node.left);
+        let rightheight = this.heightHelper(current_node.right);
+        
+        if (leftheight>= rightheight){
+            return leftheight +1
+        }
+        if (rightheight> leftheight){
+            return rightheight +1
+        }
+
+    }
+
+    height (){
+        let current_node = this.root;
+        return this.heightHelper(this.root)
+    }
+    
+    isBalanced(){
+        let left = this.heightHelper(this.root.left)
+        let right = this.heightHelper(this.root.right)
+        if (Math.abs(left-right)<=1){
+            return true
+        }
+        return false
+
+    }
 }
 
-bst1 = new BST();
-bst1.add(5).add(6).add(10).add(5);
+
+function arrayToBTS(arr) {
+    let mid = arr.length/2    
+}
+
+let bst1 = new BST();
+bst1.add(5).add(6).add(10).add(5).add(3).add(2).add(11);
 console.log(bst1) 
+console.log(bst1.isBalanced())
